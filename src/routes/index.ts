72beae543bc0784a8a5dea45b809b6f5f43fac75
DsBrito <dionatas.brito@edu.ufes.br>
2023-11-20 19:19:25 -0300

    👨‍💻 :: em desenvolvimento
    Página de Classificação em desenvolvimento

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { AiOutlineBarChart, AiOutlineUser } from 'react-icons/ai';
 import { FiFolder } from 'react-icons/fi';
 import { AiOutlineHome } from 'react-icons/ai';
 import { GiShipWheel } from 'react-icons/gi';
+import { IoPodiumOutline } from "react-icons/io5";
+
 
 export const menus = [
   { name: "Home", href: "/", icon: AiOutlineHome },
@@ -9,4 +11,6 @@ export const menus = [
   { name: "Pilotos", href: "/pilots", icon: GiShipWheel },
   { name: "Histórico", href: "/files", icon: FiFolder },
   { name: "Relatórios", href: "/relatorios", icon: AiOutlineBarChart },
+  { name: "Classificação", href: "/classificacao", icon: IoPodiumOutline }
+
 ];
